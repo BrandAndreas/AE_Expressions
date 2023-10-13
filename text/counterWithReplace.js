a866d61f5aa4dd3e2721c4d7fc50509cb1262fc5
BrandAndreas <brand.unter3@gmail.com>
2023-10-13 11:47:26 +0200

    folder deleted

--- text/counterWithReplace.js
+++
@@ -1,9 +0,0 @@
-// Counts from the first to the second marker of the layer
-
-const countFrom = 0;
-const countTo = 100;
-const decimalPlaces = 2;
-const replaceFrom = ",";
-const replaceTo = ".";
-
-linear(time, marker.key(1).time, marker.key(2).time, countFrom, countTo).toFixed(decimalPlaces).replace(replaceFrom, replaceTo);
